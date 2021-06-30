673abfeba19a5a96813666a35bfc0b58026e8112
phildesignsgfx <phildesignsgfx@gmail.com>
2021-06-30 20:46:17 +0000

    complete simple counter

--- src/js/index.js
+++ src/js/index.js
@@ -11,5 +11,16 @@ import "../styles/index.scss";
 //import your own components
 import { Home } from "./component/home.js";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(() => {
+	counter++;
+	let num1 = Math.floor(counter / 1);
+	let num2 = Math.floor(counter / 10);
+	let num3 = Math.floor(counter / 100);
+
+	//render your react application
+	ReactDOM.render(
+		<Home digit1={num1} digit2={num2} digit3={num3} />,
+		document.querySelector("#app")
+	);
+}, 1000);
